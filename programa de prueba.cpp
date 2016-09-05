6c5ad9976586c2d5533886ebde950d21ca6f5dd8
rubenjo1 <yn.ruben@gmail.com>
2016-09-05 09:55:29 -0500

    aprendiendo
    aprendiendo

---
+++ programa de prueba.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdint.h>
+#include <windows.h>
+int main(int argc, char** argv)
+{
+int a;
+printf("******** REPOSITORIO DE PRUEBA ********\n");
+printf("********  RUBEN JOSE MAURY TH. ********\n\n");
+printf("De un valor para a: ");
+	scanf("%d",&a);
+	printf("El valor de a es: %d\n\n\n ",a);
+	
+	printf("este es un  programa de prueba diseñado para la actividad #1\n");
+	printf("de el programa de formacion DISEÑO DE PAGINAS WEB\n");
+	
+	system("pause");
+	
+	return 0;
+}
